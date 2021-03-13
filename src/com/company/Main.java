2440f409c9598d3341421a928a0bbd019dc60383
alexs <alexskarlatov@gmail.com>
2021-03-13 18:14:17 -0500

    save point 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,12 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 
@@ -11,12 +18,46 @@ public class Main {
 
         //get the stuff from the files into my program
 
+        try{
+            //prompt the user for the file names and file paths
 
-//commnet
-//commnet
+            FileReader fr1 = new FileReader("enzo 1 library.txt");
+            FileReader fr2 = new FileReader("enzo 1 library.txt");
+
+            BufferedReader bufferedReader = new BufferedReader(fr1);
+            BufferedReader bufferedReader2 = new BufferedReader(fr2);
+
+            String str1, str2;
+            while ((str1 = bufferedReader.readLine())   != null ||   (str2 = bufferedReader2.readLine())  != null){
+                System.out.println("testing to see if works" + str1);
+            }
+
+//          fileReader();
+        }
+        catch(IOException e ){
+            System.out.println("eerror");
+        }
 
 
 
 
+
+    }
+    //refine this after proof of concept phase
+    private static void fileReader(ArrayList<String> librariesToCompare) throws FileNotFoundException {
+        try
+        {
+            List<FileReader> fileReaders = new ArrayList<>();
+            List<BufferedReader> bufferedReaders = new ArrayList<>();
+            for(int i = 0 ; i < librariesToCompare.size(); i++){
+                //create a bunch of file reader
+                fileReaders.add(new FileReader(librariesToCompare.get(i)));
+                bufferedReaders.add(new BufferedReader(fileReaders.get(i)));
+            }
+    //       BufferedReader br = new BufferedReader();
+        }
+        catch (IOException e){
+
+        }
     }
 }
