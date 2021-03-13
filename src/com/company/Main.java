1f6252b5af503fc403c7c057df50a02b2074ffc8
alexs <alexskarlatov@gmail.com>
2021-03-13 16:55:59 -0500

    start

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+
+    /*
+        program desscription:
+     */
+    public static void main(String[] args) {
+	// write your code here
+
+        //get the stuff from the files into my program
+
+
+
+
+
+
+
+    }
+}
