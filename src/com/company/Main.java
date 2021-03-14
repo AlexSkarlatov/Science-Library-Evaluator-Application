3150aeb54b333d4a48e534d98aeb31907b3f761c
alexs <alexskarlatov@gmail.com>
2021-03-14 18:45:21 -0400

    product now produces results, next task is to abstract it out to an external class supply arguments thorough the args command line or gbeter yet use java input reader utility use filewriter to create and email the file to a location of the user's choice turn it into a jar file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
 
@@ -21,28 +22,68 @@ public class Main {
         try{
             //prompt the user for the file names and file paths
 
-
-            FileReader fr1 = new FileReader("E:\\CODING-LEARNINGS\\SpringBootLearning\\ListUnionVerificationApp\\src\\com\\company\\textfiles\\enzo.txt");
-            FileReader fr2 = new FileReader("E:\\CODING-LEARNINGS\\SpringBootLearning\\ListUnionVerificationApp\\src\\com\\company\\textfiles\\preswick.txt");
+            FileReader fr1 = new FileReader("E:\\CODING-LEARNINGS\\SpringBootLearning\\ListUnionVerificationApp\\src\\com\\company" +
+                    "\\textfiles\\enzo.txt");
+            FileReader fr2 = new FileReader("E:\\CODING-LEARNINGS\\SpringBootLearning\\ListUnionVerificationApp" +
+                    "\\src\\com\\company\\textfiles\\preswick.txt");
+//            FileReader fr1 = new FileReader("E:\\CODING-LEARNINGS\\SpringBootLearning\\ListUnionVerificationApp\\src\\com\\company" +
+//                            "\\textfiles\\enzo.txt");
+//            FileReader fr2 = new FileReader("E:\\CODING-LEARNINGS\\SpringBootLearning\\ListUnionVerificationApp" +
+//                    "\\src\\com\\company\\textfiles\\preswick.txt");
 
             BufferedReader bufferedReader = new BufferedReader(fr1);
             BufferedReader bufferedReader2 = new BufferedReader(fr2);
 
             String str1, str2 = " ";
-            while ((str1 = bufferedReader.readLine())   != null ||   (str2 = bufferedReader2.readLine())  != null){
-                System.out.println("list 1 ===  " + str1 + "|||    list 2 ===   " + str2 );
+            List<String> libraryAlpha = new ArrayList<>();
+            List<String> libraryBeta = new ArrayList<>();
+
+            //fill up list alpha
+            while ((str1 = bufferedReader.readLine())   != null){
+                libraryAlpha.add(str1);
             }
+            while ((str2 = bufferedReader2.readLine())   != null){
+                libraryBeta.add(str2);
+            }
+//            System.out.println("length of list alpha == " + libraryAlpha.size() + "  []  length of list beta == "
+//                    + libraryBeta.toString());
+//            System.out.println("  HEEEEEEEEEEEEEEEERE "+libraryBeta.get(5).toLowerCase());
+            List<String> output = new ArrayList<>();
+            int count = 0;
+            for(int i = 0 ; i < libraryAlpha.size(); i++){
+                for(int j = 0; j < libraryBeta.size(); j++){
+                    String sAlpha, sBeta;
+
+                    sAlpha = libraryAlpha.get(i).toLowerCase();
+                    sBeta = libraryBeta.get(j).toLowerCase();
+//                    System.out.println(  sAlpha + "   versus    " + sBeta);
+                    if( sAlpha.equalsIgnoreCase(sBeta) ){
+                        //add this to the result list
+                        output.add(sAlpha);
+//                        System.out.println("adding salpha" +  sAlpha + "string output here "+output.toString());
+//                        System.out.println("iteration number" + count++);
+                        break;
+                    }
+//                    System.out.println("iteration number" + count++);
+                }
+
+//                System.out.println(output);
+            }
+            System.out.println("==============================================================================");
+            for(int i = 0; i < output.size(); i++){
+                System.out.println(output.get(i));
+//                System.out.println(output.size());
+            }
+
+//            while ((str1 = bufferedReader.readLine())   != null ||   (str2 = bufferedReader2.readLine())  != null){
+//                System.out.println("list 1 ===  " + str1 + "|||    list 2 ===   " + str2 );
+//            }
 
 //          fileReader();
         }
         catch(IOException e ){
             e.printStackTrace();
         }
-
-
-
-
-
     }
     //refine this after proof of concept phase
     private static void fileReader(ArrayList<String> librariesToCompare) throws FileNotFoundException {
